746112274edae48c1a4ef85d2c6a6bac313a0659
Sebastian Meza-Torres <seb2131176@maricopa.edu>
2026-04-21 18:04:39 -0700

    Updated HomePage
    I added a dark mode feature for the homepage and the night mode worked, but it pushed away the links and I can't figure out why it is doing that. I ended up commenting them out.

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ const navLinks = document.querySelector(" .nav-links");
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("open");
 });
+
+const darkModeBtn = document.getElementById("darkModeBtn");
+
+darkModeBtn.addEventListener("change", () => {
+  const isDark = darkModeBtn.checked;
+  document.body.classList.toggle("dark", isDark);
+  localStorage.setItem("darkMode", isDark);
+});
+
+if (localStorage.getItem("darkMode") === "true") {
+  document.body.classList.add("dark");
+  darkModeBtn.checked = true;
+}
